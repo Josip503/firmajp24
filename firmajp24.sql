299eedd504037b37fa4066645ee5d5ee1068b32e
Josip Conjar <josip.conjar@gmail.com>
2021-05-13 12:53:25 +0200

    kreiranje tablica

--- firmajp24.sql
+++ firmajp24.sql
@@ -1,2 +1,32 @@
-DROP database IF EXISTS firmajp24;
-CREATE database firmajp24;
+drop database IF EXISTS firmajp24;
+create database firmajp24;
+use firmajp24;
+
+create table projekt(
+	sifra int NOT NULL PRIMARY KEY auto_increment,
+	naziv varchar(50) not null,
+	cijena decimal(18,2)
+	);
+	
+create table programer(
+	sifra int not null primary key auto_increment,
+	ime varchar(50) not null,
+	prezime varchar(50) not null,
+	datumrodenja datetime,
+	placa decimal(18,2)
+	);
+	
+create table sudjeluje(
+	projekt int not null,
+	programer int not null,
+	datumpocetka datetime not null,
+	datumkraja datetime
+	);
+	
+alter table sudjeluje add foreign key (projekt) references projekt(sifra);
+alter table sudjeluje add foreign key (programer) references programer(sifra);
+
+
+
+
+
